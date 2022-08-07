c188afcad1d6ed9dab2f60e9060acd23c7a54fd2
prince kehinde <princekehinde2345@gmail.com>
2022-08-07 07:07:58 -0700

    mongodb connected

--- config/key.js
+++ config/key.js
@@ -1,7 +1,6 @@
-// require("dotenv").config();
-
-// module.exports = {
-//   MongoDB: process.env.MONGO_URL,
-//   PORT: process.env.PORT,
-//   PASS_SEC: process.env.PASS_SEC,
-// };
+ require("dotenv").config();
+ 
+module.exports = {
+MongoDB: process.env.MONGO_URL,
+TEST_DB: process.env.TEST_URL,
+};
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const server = require('./routes/index')
-const db = require('./config/db')
+const data = require('./config/db')
 const Port = process.env.PORT || 3000
 
+
+data()
+.then(() =>
+{
+    console.log('Connected to MongoDB')
+})
+.catch(err =>{ console.log(err)
+
+})
 
 
 server.listen(Port, () => {
